9e782c3d78fa0d6f7159fa99f44c4f167d6c7941
PranavMoralwar123 <150120819+PranavMoralwar123@users.noreply.github.com>
2026-08-11 12:22:12 +0530

    feat: add safe environment configuration.

---
+++ app.py
@@ -0,0 +1,11 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY =os.getenv("API_KEY")
+
+if not API_KEY:
+    raise RuntimeError("API_KEY is not configured.")
+
+print("API KEY loaded successfully.")
